b19dec1ce11eaa80e5a8365c6d91e528ea045b73
Shawn Soneja <shawn.soneja85@gmail.com>
2024-08-24 21:14:42 -0400

    Updating Nav Bar - changing to my name and preserving page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,8 +38,8 @@ const Navigation = React.forwardRef((props, ref) => {
         }`}
       expand="lg"
     >
-      <Navbar.Brand className="navbar-brand" href={process.env.PUBLIC_URL + "/#home"}>
-        {`<${mainBody.firstName} />`}
+      <Navbar.Brand className="navbar-brand" href={"#home"}>
+        {`${'Shawn Soneja'}`}
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" className="toggler" />
       <Navbar.Collapse id="basic-navbar-nav">
@@ -52,7 +52,7 @@ const Navigation = React.forwardRef((props, ref) => {
           {repos.show && (
 
             <NavLink
-              href={process.env.PUBLIC_URL + "/#projects"}
+              href={process.env.PUBLIC_URL + "#projects"}
             >
               Projects
             </NavLink>
@@ -68,7 +68,7 @@ const Navigation = React.forwardRef((props, ref) => {
           {about.show && (
             <NavLink
               className="nav-item lead"
-              href={process.env.PUBLIC_URL + "/#aboutme"}
+              href={process.env.PUBLIC_URL + "#aboutme"}
             >
               About
             </NavLink>
@@ -76,7 +76,7 @@ const Navigation = React.forwardRef((props, ref) => {
           {skills.show && (
             <NavLink
               className="nav-item lead"
-              href={process.env.PUBLIC_URL + "/#skills"}
+              href={process.env.PUBLIC_URL + "#skills"}
             >
               Skills
             </NavLink>
